7903bfdacc1be5d213202d3652cd4b3ef402d03d
leeowonikoko <leeowonikoko@yahoo.com>
2022-02-23 12:01:49 +0100

    left menu complete

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BsFillVolumeUpFill, BsMusicNoteList } from 'react-icons/bs'
+import { FaDesktop } from 'react-icons/fa'
+
 
 const TrackList = () => {
   return (
-    <div>TrackList</div>
+    <div className='tracklist'>
+      <div className='top'>
+        <img src='https://raw.githubusercontent.com/Vetrivel-VP/spotify-clone/master/src/img/track.png' alt='trackImg' />
+        <p className='trackName'>Essence<span className='trackSpan'>Wizkid</span></p>
+      </div>
+
+      <div className='bottom'>
+        <i><BsFillVolumeUpFill /></i>
+        <input type="range"  />
+        <i><BsMusicNoteList /></i>
+        <i><FaDesktop /></i>
+      </div>
+    </div>
   )
 }
 
