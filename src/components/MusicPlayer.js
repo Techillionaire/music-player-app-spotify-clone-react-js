42c8dc508c26f86a17b0a7cb21addb33a44c4c7c
leeowonikoko <leeowonikoko@yahoo.com>
2022-03-10 18:31:48 +0100

    g

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { FaBackward, FaForward, FaHeart, FaPause, FaPlay, FaRegHeart, FaShareAlt, FaStepBackward, FaStepForward } from 'react-icons/fa'
 import { BsDownload } from 'react-icons/bs'
 import '../styles/musicPlayer.css'
@@ -7,23 +7,46 @@ const MusicPlayer = ({song, imgSrc}) => {
 
     const [isLove, setIsLove] = useState(false);
     const [ isPlaying, setIsPlaying ] = useState(false);
+    const [duration, setDuration] = useState(0)
+
+
+    const audioPlayer = useRef();  // our audiotags
+    const progressBar = useRef();  // our progressbar
+
+    useEffect(()=> {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds)
+    }, 
+    [
+        audioPlayer?.current?.loadedmetadata, 
+        audioPlayer?.current?.readyState,
+    ])
 
     const changeLoved = () => {
         setIsLove(!isLove)
+        
     }
 
     const changePlayPause = () => {
+        const prevValue = isPlaying;
+        
+        if(!prevValue){
+            audioPlayer.current.play();
+        } else {
+            audioPlayer.current.pause();
+        }
+
         setIsPlaying(!isPlaying)
     }
 
   return (
     <div className='musicPlayer'>
         <div className='songImage'>
-            
+            <img src={imgSrc} alt='' />
         </div>
 
         <div className='songAttributes'>
-            <audio src={song} preload='metadata' />
+            <audio src={song} preload='metadata' ref={audioPlayer}/>
 
             <div className='top'>
                 <div className='left'>
@@ -68,7 +91,9 @@ const MusicPlayer = ({song, imgSrc}) => {
             </div>
 
             <div className='bottom'>
-
+               <div className='time'>00:00</div>
+                <input type='range' className='progressBar' ref={progressBar}/>
+                <div className='durantion'>{duration}</div>
             </div>
         </div>
     </div>
