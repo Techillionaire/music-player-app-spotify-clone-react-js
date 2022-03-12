376cc6fb39d1e1b0f8c36c9173d78165f298887a
leeowonikoko <leeowonikoko@yahoo.com>
2022-03-12 12:57:45 +0100

    app 100% complete wheeeewwwww

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import '../styles/rightmenu.css'
+import { FaCrown, FaBell, FaRegHeart, FaSun, FaCogs } from 'react-icons/fa'
 
 const RightMenu = () => {
   return (
-    <div className='rightMenu'>RightMenu</div>
+    <div className='rightMenu'>
+      <div className='goPro'>
+        <i>
+          <FaCrown />
+          <p>Go <span>Pro</span></p>
+        </i>
+        <i>
+          <FaBell />
+        </i>
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+
+      <div className='profile'>
+        <i>
+          <FaSun />
+        </i>
+        <i>
+          <FaCogs />
+        </i>
+
+        <div className='profileImage'>
+          <img src='https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmask.jpg?alt=media&token=22f0d7be-74b8-4c0d-849d-b777370e9559' alt='' />
+        </div>
+      </div>
+        
+    </div>
   )
 }
 
